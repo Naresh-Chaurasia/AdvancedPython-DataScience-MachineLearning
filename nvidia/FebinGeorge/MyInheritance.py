a4edd10ed84d5ac1c6f965994adb41fec576060e
connect2tech <message4naresh@gmail.com>
2021-10-10 15:06:17 +0530

    Lenovo Ideapad 320, Checkin Timestamp :: `date +'m-H:S'`;

--- nvidia/FebinGeorge/MyInheritance.py
+++ nvidia/FebinGeorge/MyInheritance.py
@@ -1,5 +1,10 @@
 class Furniture:
     color="brown"
+
+    publicAttribute='I am Public'
+    _protectedAttribute = 'I am Protected'
+    __privateAttribute = "I am Private"
+
     def __init__(self):
         print('Inside __init__ @Furniture')
     
@@ -7,6 +12,7 @@ class Furniture:
         print('furnitureDetail')
 
 class Chair(Furniture):
+    
     def __init__(self):
         print('Inside __init__ @Chair')
         self.chairColor="brownish"
@@ -16,8 +22,14 @@ class Chair(Furniture):
         print('self.color=',self.color)
         print('Furniture.color=',Furniture.color)
 
+    def displayAttributes(self):
+        print('self._protectedAttribute',self._protectedAttribute)
+        print('self.__privateAttribute',self._Furniture__privateAttribute)
 
+fur = Furniture()
 c = Chair()
-c.chairDetails()
-c.furnitureDetail()
-print(c.color)
+# c.chairDetails()
+# c.furnitureDetail()
+# print(c.color)
+c.displayAttributes()
+#print(c._Furniture__privateAttribute)
---
+++ nvidia/FebinGeorge/MyOverride.py
@@ -0,0 +1,27 @@
+class Car:
+    def startEngine(self):
+        self.power = 100
+
+    def printEngine(self):
+        print('printEngine::', self.power)
+
+class Maruti(Car):
+    def startEngine(self):
+        print('startEngine2')
+        self.power = 200
+
+    def engineReset(self):
+        super().startEngine()
+
+        
+
+c = Car()
+c.startEngine()
+c.printEngine()
+
+c2 = Maruti()
+c2.startEngine()
+c2.printEngine()
+c2.engineReset()
+c2.printEngine()
+    
